37b2df8891077678a374d6afd8434835a5806bf7
Jacobo <jacoborodicio@gmail.com>
2021-11-10 23:21:28 +0100

    chore: rename module name in production

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -13,7 +13,7 @@ const prodConfig = {
     },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'code-school',
+            name: 'code_school',
             filename: 'remoteEntry.js',
             exposes: {
                 './codeSchoolApp': './src/bootstrap'
